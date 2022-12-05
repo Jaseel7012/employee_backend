ece8eac9fb6898703efec2e8846362c2c316debd
jaseel <jaseeljctcs@gmail.com>
2022-12-05 16:14:18 +0530

    initial

---
+++ src/main/java/com/example/employee2_backend/Employee2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employee2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employee2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee2_backend/Employee2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employee2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
