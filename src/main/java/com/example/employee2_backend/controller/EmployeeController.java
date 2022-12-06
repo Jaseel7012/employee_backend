b85dd0fc75215a215be9a341a9b6096fcf1747ab
jaseel <jaseeljctcs@gmail.com>
2022-12-06 10:53:27 +0530

    employee add

--- src/main/java/com/example/employee2_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employee2_backend/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.example.employee2_backend.controller;
 
+import com.example.employee2_backend.Models.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,24 +12,26 @@ public class EmployeeController {
     public String HomePage(){
         return "home page";
     }
-    @GetMapping("/add")
-    public String EmployeeAdd(){
+    @PostMapping (path="/add",consumes = "application/json" , produces = "application/json")
+    public String EmployeeAdd(@RequestBody Employee e){
+        System.out.println(e.getUsername().toString());
         return  "employee add page";
     }
+
     @GetMapping("/view")
 
     public String EmployeeView(){
         return  "Employee view";
     }
-    @GetMapping("/delete")
+    @PostMapping("/delete")
     public String EmployeeDelete(){
         return  "delete employee page";
     }
-    @GetMapping("/edit")
+    @PostMapping("/edit")
     public  String EmployeeEdit(){
         return  "edit page";
     }
-    @GetMapping("/search")
+    @PostMapping("/search")
     public  String EmployeeSearch(){
         return "search page";
     }
